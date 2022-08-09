71375229e9dbf988a497071e12bd03b31a42bc16
frasfras <65541080+frasfras@users.noreply.github.com>
2022-08-09 17:45:31 +0800

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <HelloWorld class="knob-label" msg="Bnb Visual Analyzer " />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "../components/PlanView";
+//import HelloWorld from "../components/BnbDeal";
+/* eslint-disable */ 
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
+<style>
+knob-label {
+    display: block;
+    margin-bottom: 0;
+    font-weight: bold;
+     color: #febc2c;
+}
+</style>
